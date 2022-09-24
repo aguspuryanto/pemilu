f87329729d8ec23bc04780b282e1852d9bb3c4e5
Agus Puryanto <aguspuryanto@gmail.com>
2022-09-24 13:51:18 +0700

    update logout

--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -51,7 +51,9 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
-      
+      <li class="nav-item">
+        <a class="nav-link" href="<?=base_url('Dashboard1/logout');?>" role="button"><i class="fas fa-sign-out-alt"></i> Logout</a>
+      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
